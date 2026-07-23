e8d73762c4085b4d051897c9ae2190b1eb7e0178
jakob <me@example.com>
2026-07-23 19:08:08 +0200

    cleaned main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,5 @@
 #include "View/mainwindow.h"
-#include "classes/User.class.h"
-#include "View/userview.h"
-#include "classes/Stock.class.h"
-#include "classes/Account.class.h"
-#include "ViewModel/accountviewmodel.h"
-#include "ViewModel/userviewmodel.h"
-#include "View/accountview.h"
-#include "View/stockview.h"
-#include "View/stockview.h"
-#include <QSqlDatabase>
-#include <QSqlQuery>
-#include <QSqlError>
-#include <QDebug>
 #include <QApplication>
-#include <QTextStream>
-#include <iostream>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
